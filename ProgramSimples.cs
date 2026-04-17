ca75f6012ffa0b9236239918e36539664c5896d9
Alicetxs4 <maria.a.silva31@edu.senai.br>
2026-04-17 08:18:20 -0300

    Add files via upload

---
+++ ProgramSimples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    class program
+    {
+        
+        static void Main (string[] args)
+        {
+            
+            Console.WriteLine("---Calculadora SENAI---");
+
+            Console.Write("Digite o primeiro número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Realiza a soma e exibe o resultado
+            double soma = num1 + num2;
+            Console.WriteLine($"O resultado da soma é: {soma}");
+        }
+    }
+}
